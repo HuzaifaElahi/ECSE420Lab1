16a44e685616dae0d8f1669e2faaa6128c2cc1b5
Ahmed Elehwany <ahmed.elehwany@mail.mcgill.ca>
2020-10-17 18:46:20 -0400

    Refactor parallel logic

--- parallel_explicit.cu
+++ parallel_explicit.cu
@@ -21,7 +21,7 @@ inline cudaError_t checkCudaErr(cudaError_t err, const char* msg) {
 }
 
 
-__global__ void doLogicGates(char* inputBuffer, int inputLength, char* outputBuffer, int inputLineLength, int outputLineLength) {
+__global__ void evaluateLogicGate(char* inputBuffer, int inputLength, char* outputBuffer, int inputLineLength, int outputLineLength) {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
 
     if (index < inputLength) {
@@ -29,35 +29,32 @@ __global__ void doLogicGates(char* inputBuffer, int inputLength, char* outputBuf
 
         int operand1 = logicLine[0] - '0';
         int operand2 = logicLine[2] - '0';
-        int operatorr = logicLine[4] - '0';
-        int output;
+        int gateType = logicLine[4] - '0';
+        char result;
 
-        switch (operatorr) {
-        case 0:
-            output = operand1 & operand2;
+        switch (gateType) {
+        case NOR:
+            result = !(operand1 | operand2);
             break;
-        case 1:
-            output = operand1 | operand2;
+        case XOR:
+            result = operand1 ^ operand2;
             break;
-        case 2:
-            output = !(operand1 & operand2);
+        case NAND:
+            result = !(operand1 & operand2);
             break;
-        case 3:
-            output = !(operand1 | operand2);
+        case AND:
+            result = operand1 & operand2;
             break;
-        case 4:
-            output = operand1 ^ operand2;
+        case OR:
+            result = operand1 | operand2;
             break;
-        case 5:
-            output = !(operand1 ^ operand2);
+        case NXOR:
+            result = !(operand1 ^ operand2);
             break;
         }
 
-
-
         char* outputLocation = outputBuffer + index * outputLineLength;
-        char outputValue = output + '0';
-
+        char outputValue = result + '0';
 
         outputLocation[0] = outputValue;
         outputLocation[1] = '\0';
@@ -65,99 +62,97 @@ __global__ void doLogicGates(char* inputBuffer, int inputLength, char* outputBuf
     }
 }
 
-//int main(int argc, char* argv[]) {
-//
-//    FILE* inputFile;
-//    FILE* outputFile;
-//
-//    int inputLineLength = 7;
-//    int outputLineLength = 3;
-//
-//    cudaEvent_t start, stop;
-//    cudaEventCreate(&start);
-//    cudaEventCreate(&stop);
-//    float milliseconds = 0;
-//    float copyMilliseconds = 0;
-//
-//    if (argc != 4) {
-//        printf("Error: Please enter the input file path, input file length and output file path when running.\n");
-//        return 1;
-//    }
-//
-//    char* input_fileName = argv[1];
-//    int inputLength = atoi(argv[2]);
-//    char* output_fileName = argv[3];
-//
-//    inputFile = fopen(input_fileName, "r");
-//    if (inputFile == NULL) {
-//        fprintf(stderr, "Error opening file.\n");
-//        return 1;
-//    }
-//
-//    int inputSize = inputLength * inputLineLength * sizeof(unsigned char);
-//    int outputSize = inputLength * outputLineLength * sizeof(unsigned char);
-//
-//    char* inputBuffer = (char*)malloc(inputSize);
-//
-//    char buf[7];
-//    int addressLocation = 0;
-//    while (fgets(buf, sizeof buf, inputFile) != NULL) {
-//        strcpy(inputBuffer + addressLocation, buf);
-//        addressLocation += inputLineLength;
-//    }
-//
-//    fclose(inputFile);
-//
-//    char* cudaBuffer;
-//    char* outputBuffer;
-//    char* returnBuffer;
-//    returnBuffer = (char*)malloc(outputSize * sizeof(char));
-//
-//    cudaMalloc(&cudaBuffer, inputSize);
-//    cudaMalloc(&outputBuffer, outputSize);
-//
-//
-//    clock_t start_copy = clock();
-//    cudaMemcpy(cudaBuffer, inputBuffer, inputSize, cudaMemcpyHostToDevice);
-//    clock_t end_copy = clock();
-//    float time_spent = ((end_copy - start_copy) * 1000.0) / CLOCKS_PER_SEC;
-//    printf("Copying Time elapsed: %.6fms\n", time_spent);
-//
-//    // Run kernel and record time
-//    cudaEventRecord(start);
-//    doLogicGates << < (inputLength + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK >> > (cudaBuffer, inputLength, outputBuffer, inputLineLength, outputLineLength);
-//
-//    cudaEventRecord(stop);
-//    cudaMemcpy(returnBuffer, outputBuffer, outputSize, cudaMemcpyDeviceToHost);
-//    cudaEventSynchronize(stop);
-//
-//    checkCudaErr(cudaDeviceSynchronize(), "Syncronization");
-//    checkCudaErr(cudaGetLastError(), "GPU");
-//
-//    cudaEventElapsedTime(&milliseconds, start, stop);
-//    cudaEventDestroy(start);
-//    cudaEventDestroy(stop);
-//
-//    outputFile = fopen(output_fileName, "w");
-//    int counter = 0;
-//    addressLocation = 0;
-//
-//    while (counter < inputLength) {
-//        if (counter == inputLength - 1) {
-//            fprintf(outputFile, "%s", returnBuffer + addressLocation);
-//        }
-//        else {
-//            fprintf(outputFile, "%s\n", returnBuffer + addressLocation);
-//        }
-//        addressLocation += outputLineLength;
-//        counter++;
-//    }
-//
-//    fclose(outputFile);
-//
-//
-//    printf("Time elapsed: %.6fms\n", milliseconds);
-//    printf("Success!\n");
-//
-//    return 0;
-//}
+/*
+* Parallel computation of list of logical expressions.
+* Takes input file, file length and output file name as arguments.
+*/
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        printf("Error: Please enter the input file path, input file length and output file path when running.\n");
+        return 1;
+    }
+
+    int inputLineLength = 7;
+    int outputLineLength = 3;
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    char* input_fileName = argv[1];
+    int inputLength = atoi(argv[2]);
+    char* output_fileName = argv[3];
+
+    FILE* inputFile = fopen(input_fileName, "r");
+    if (inputFile == NULL) {
+        fprintf(stderr, "Error opening file.\n");
+        return 1;
+    }
+
+    int inputSize = inputLength * inputLineLength * sizeof(unsigned char);
+    int outputSize = inputLength * outputLineLength * sizeof(unsigned char);
+
+    char* inputBuffer = (char*)malloc(inputSize);
+
+    char buf[7];
+    int addressLocation = 0;
+    while (fgets(buf, sizeof buf, inputFile) != NULL) {
+        strcpy(inputBuffer + addressLocation, buf);
+        addressLocation += inputLineLength;
+    }
+
+    fclose(inputFile);
+
+    char* cudaBuffer;
+    char* outputBuffer;
+    char* returnBuffer;
+    returnBuffer = (char*)malloc(outputSize * sizeof(char));
+
+    cudaMalloc(&cudaBuffer, inputSize);
+    cudaMalloc(&outputBuffer, outputSize);
+
+
+    clock_t copyStart = clock();
+    cudaMemcpy(cudaBuffer, inputBuffer, inputSize, cudaMemcpyHostToDevice);
+    clock_t copyEnd = clock();
+    float copyDuration = ((copyEnd - copyStart) * 1000.0) / CLOCKS_PER_SEC;
+    printf("Copying Completed: %.6fms\n", copyDuration);
+
+    // Run kernel and record time
+    cudaEventRecord(start);
+    evaluateLogicGate << < (inputLength + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK >> > (cudaBuffer, inputLength, outputBuffer, inputLineLength, outputLineLength);
+
+    cudaEventRecord(stop);
+    cudaMemcpy(returnBuffer, outputBuffer, outputSize, cudaMemcpyDeviceToHost);
+    cudaEventSynchronize(stop);
+
+    checkCudaErr(cudaDeviceSynchronize(), "Syncronization");
+    checkCudaErr(cudaGetLastError(), "GPU");
+
+    float duration = 0;
+    cudaEventElapsedTime(&duration, start, stop);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    FILE* outputFile = fopen(output_fileName, "w");
+    int counter = 0;
+    addressLocation = 0;
+
+    while (counter < inputLength) {
+        if (counter == inputLength - 1) {
+            fprintf(outputFile, "%s", returnBuffer + addressLocation);
+        }
+        else {
+            fprintf(outputFile, "%s\n", returnBuffer + addressLocation);
+        }
+        addressLocation += outputLineLength;
+        counter++;
+    }
+
+    fclose(outputFile);
+
+    printf("Completed!\n");
+    printf("Time: %.6fms\n", duration);
+
+    return 0;
+}
--- sequential.cu
+++ sequential.cu
@@ -1,89 +1,89 @@
-//#include <string>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <time.h>
-//
-//#define AND 0
-//#define OR 1
-//#define NAND 2
-//#define NOR 3
-//#define XOR 4
-//#define NXOR 5
-//
-///*
-//* Evaluates logical expression.
-//*/
-//char evaluateLogicGate(int ops[]) {
-//    int result=0;
-//
-//    switch (ops[2]) {
-//    case NOR:
-//        result = !(ops[0] | ops[1]);
-//        break;
-//    case XOR:
-//        result = ops[0] ^ ops[1];
-//        break;
-//    case NAND:
-//        result = !(ops[0] & ops[1]);
-//        break;
-//    case AND:
-//        result = ops[0] & ops[1];
-//        break;
-//    case OR:
-//        result = ops[0] | ops[1];
-//        break;
-//    case NXOR:
-//        result = !(ops[0] ^ ops[1]);
-//        break;
-//    }
-//
-//    char output = result + '0';
-//    return output;
-//
-//}
-//
-///*
-//* Sequential computation of list of logical expressions.
-//* Takes input file, file length and output file name as arguments.
-//*/
-//int main(int argc, char* argv[]) {
-//    if (argc != 4) {
-//        printf("Error: Please enter the input file path, input file length and output file path when running.\n");
-//        return 1;
-//    }
-//    char line[10];
-//    char* inputFileName = argv[1];
-//    int inputLength = atoi(argv[2]);
-//    char* outputFileName = argv[3];
-//
-//    FILE* inputFile = fopen(inputFileName, "r");
-//    FILE* outputFile = fopen(outputFileName, "w");
-//
-//    if (inputFile == NULL) {
-//        fprintf(stderr, "Error opening file.\n");
-//        return 1;
-//    }
-//
-//    clock_t start = clock();
-//
-//    int operands[3] = { 0 };
-//    char result = NULL;
-//    while (fgets(line, 10, inputFile)) {
-//        operands[0] = atoi(line);
-//        operands[1] = atoi(line + 2);
-//        operands[2] = atoi(line + 4);
-//        result = evaluateLogicGate(operands);
-//        fprintf(outputFile, "%c\n", result);
-//
-//    }
-//    clock_t end = clock();
-//    int duration = ((end - start) * 1000) / CLOCKS_PER_SEC;
-//
-//    fclose(outputFile);
-//    fclose(inputFile);
-//
-//    printf("Completed\nTime: %ds %dms\n", duration / 1000, duration % 1000);
-//
-//    return 0;
-//
-//}
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define AND 0
+#define OR 1
+#define NAND 2
+#define NOR 3
+#define XOR 4
+#define NXOR 5
+
+/*
+* Evaluates logical expression.
+*/
+char evaluateLogicGate(int ops[]) {
+    int result=0;
+
+    switch (ops[2]) {
+    case NOR:
+        result = !(ops[0] | ops[1]);
+        break;
+    case XOR:
+        result = ops[0] ^ ops[1];
+        break;
+    case NAND:
+        result = !(ops[0] & ops[1]);
+        break;
+    case AND:
+        result = ops[0] & ops[1];
+        break;
+    case OR:
+        result = ops[0] | ops[1];
+        break;
+    case NXOR:
+        result = !(ops[0] ^ ops[1]);
+        break;
+    }
+
+    char output = result + '0';
+    return output;
+
+}
+
+/*
+* Sequential computation of list of logical expressions.
+* Takes input file, file length and output file name as arguments.
+*/
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        printf("Error: Please enter the input file path, input file length and output file path when running.\n");
+        return 1;
+    }
+    char line[10];
+    char* inputFileName = argv[1];
+    int inputLength = atoi(argv[2]);
+    char* outputFileName = argv[3];
+
+    FILE* inputFile = fopen(inputFileName, "r");
+    FILE* outputFile = fopen(outputFileName, "w");
+
+    if (inputFile == NULL) {
+        fprintf(stderr, "Error opening file.\n");
+        return 1;
+    }
+
+    clock_t start = clock();
+
+    int operands[3] = { 0 };
+    char result = NULL;
+    while (fgets(line, 10, inputFile)) {
+        operands[0] = atoi(line);
+        operands[1] = atoi(line + 2);
+        operands[2] = atoi(line + 4);
+        result = evaluateLogicGate(operands);
+        fprintf(outputFile, "%c\n", result);
+
+    }
+    clock_t end = clock();
+    int duration = ((end - start) * 1000) / CLOCKS_PER_SEC;
+
+    fclose(outputFile);
+    fclose(inputFile);
+
+    printf("Completed\nTime: %ds %dms\n", duration / 1000, duration % 1000);
+
+    return 0;
+
+}
